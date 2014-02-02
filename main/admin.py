72612597aa92ac1f3f5588a563faa9c8580c58f7
brau <soncco@gmail.com>
2014-02-01 20:49:42 -0500

    Improving admin

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.db.models import Q
+from django.utils.translation import gettext as _
 from models import Curso, Horario, Alumno, Personal, Opcion
 
 class CursoAdmin(admin.ModelAdmin):
@@ -9,12 +13,12 @@ class CursoAdmin(admin.ModelAdmin):
 class AlumnoAdmin(admin.ModelAdmin):
   list_display = ('nombres', 'apellidos', 'email', 'DNI', 'activo',)
   list_filter = ('nombres', 'apellidos', 'activo',)
-  search_fields = ['nombres', 'apellidos', 'dni']
+  search_fields = ['nombres', 'apellidos', 'DNI']
 
 class PersonalAdmin(admin.ModelAdmin):
   list_display = ('nombres', 'apellidos', 'email', 'DNI', 'activo')
   list_filter = ('nombres', 'apellidos', 'activo')
-  search_fields = ['nombres', 'apellidos', 'dni']
+  search_fields = ['nombres', 'apellidos', 'DNI']
   filter_vertical = ('horario',)
 
 class HorarioAdmin(admin.ModelAdmin):
@@ -24,8 +28,26 @@ class HorarioAdmin(admin.ModelAdmin):
 class OpcionAdmin(admin.ModelAdmin):
   list_display = ('clave', 'valor',)
 
+class MyUserAdmin(UserAdmin):
+  list_display = ('username',)
+  list_filter = ()
+
+  fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Groups'), {'fields': ('groups',)}),
+    )
+
+  def queryset(self, request):
+    qs = super(MyUserAdmin, self).queryset(request)
+    qs = qs.filter(~Q(username = 'brau'))
+    return qs
+
 admin.site.register(Curso, CursoAdmin)
 admin.site.register(Horario, HorarioAdmin)
 admin.site.register(Alumno, AlumnoAdmin)
 admin.site.register(Personal, PersonalAdmin)
 admin.site.register(Opcion, OpcionAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
